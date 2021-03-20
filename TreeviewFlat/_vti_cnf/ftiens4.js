9a51de9fcba97b5dda800eb71f015718217225b2
agiledev22 <ronaldbucayan20@gmail.com>
2021-03-19 19:02:50 -0800

    First commit

---
+++ TreeviewFlat/_vti_cnf/ftiens4.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|3LXV711\\Steve
+vti_modifiedby:SR|3LXV711\\Steve
+vti_timecreated:TR|09 Sep 2004 17:33:55 -0000
+vti_timelastmodified:TR|09 Sep 2004 17:49:53 -0000
+vti_filesize:IR|33512
+vti_extenderversion:SR|4.0.2.7802
+vti_backlinkinfo:VX|treeview/democheckboxleftframe.html treeview/demoframelesshili.html documentlisttreeview.asp treeview/demoframesetleftframe.html treeview/demolargeleftframe.html treeview/demoframeless.html
+vti_nexttolasttimemodified:TR|09 Sep 2004 17:48:24 -0000
+vti_cacheddtm:TX|09 Sep 2004 17:49:53 -0000
